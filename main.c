4355ed68dbd5675b1ae64a520a0b152476aab85c
OtavioOliveira <otavio.cristhian93@gmail.com>
2024-09-09 00:43:12 -0300

    adição de codigo fonte

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 typedef struct usuario{
-    char senha[50],nome[50];
+    char RA[50],nome[50];
     int idade;
 };
 typedef struct admin{
@@ -25,10 +25,11 @@ typedef struct livro{
 
    printf(".....Bem Vindo.....\n");
    printf("Para Biblioteca [1]\n");
-   printf("Para sair [0]");
+   printf("Para sair [0]\n");
    scanf("%d", &deci1);
     system("cls");
    if(deci1 == 1){
+    printf("\n***Voce esta no meunu principal***\n");
     printf("Para cadastrar admins[1]\n");
     printf("Para cadastrar usuarios[2]\n");
     printf("Para cadastrar livros[3]\n");
@@ -46,13 +47,43 @@ typedef struct livro{
     }
     switch(deci2){
         case 1:
-            printf("***cadastro de Admin***\n");
+            system("cls");
+            printf("***Cadastro de Admin***\n");
             printf("digite o nome: ");
             scanf(" %[^\n]s", &admins.nome);
-            printf("digite uma senha");
+            printf("digite uma senha: ");
             scanf(" %[^\n]s", &admins.senha);
-            printf("***cadastro realizado com sucesso**\n");
-            
+            printf("***cadastro realizado com sucesso***\n");
+            break;
+        case 2:
+            system("cls");
+            printf("***Cadastro de usuario***\n");
+            printf("Digite o nome:");
+            scanf(" %[^\n]s", &usuarios.nome);
+            printf("Digite o RA : ");
+            scanf(" %[^\n]s", &usuarios.RA);
+            printf("***usuario cadastrado com sucesso***\n");
+            break;
+        case 3:
+            system("cls");
+            printf("***Cadastro de livro***\n");
+            printf("Nome do livro: ");
+            scanf(" %[^\n]s", &livros.nome);
+            printf("Nome do autor: ");
+            scanf(" %[^\n]s", &livros.nomeA);
+            printf("Codigo do livro: ");
+            scanf(" %[^\n]s", &livros.codigo);
+            printf("***Livro cadastrado com sucesso***\n");
+            break;
+        case 4:
+            system("cls");
+            printf("***Digite nome e senha***\n");
+            printf("digite nome : ");
+            printf("digite a senha : ");
+
+
+
+
         
 
 
